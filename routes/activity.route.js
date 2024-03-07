f5dd89c1fa502329369f4b03f996950940227d4c
shwetabhadke12 <shweta.bhadke@accelgrowthtech.com>
2024-03-07 16:43:16 +0530

    timeline section

--- routes/activity.route.js
+++ routes/activity.route.js
@@ -45,7 +45,7 @@ router.get('/tasks/:id', async (req, res) => {
 router.get('/meetings/:id', async (req, res) => {
     try {
         const data = await Meeting.find({userId:req.params.id});
-        const data = await Meeting.find({userId:req.params.id});
+      
         res.status(200).json(data);
     } catch (error) {
         console.error(error);
@@ -56,7 +56,7 @@ router.get('/meetings/:id', async (req, res) => {
 router.get('/calls/:id', async (req, res) => {
     try {
         const data = await Call.find({userId:req.params.id});
-        const data = await Call.find({userId:req.params.id});
+   
         res.status(200).json(data);
     } catch (error) {
         console.error(error);
@@ -134,7 +134,7 @@ router.post('/tasks/closetask', async (req, res) => {
  
   router.get('/tasks/closetask/:id', async (req, res) => {
     try {
-        const data = await CloseTask.find({userId:req.params.id});
+        
         const data = await CloseTask.find({userId:req.params.id});
         res.status(200).json(data);
     } catch (error) {
@@ -145,7 +145,7 @@ router.post('/tasks/closetask', async (req, res) => {
  
 router.get('/meetings/closemeeting/:id', async (req, res) => {
     try {
-        const data = await CloseMeeting.find({userId:req.params.id});
+     
         const data = await CloseMeeting.find({userId:req.params.id});
         res.status(200).json(data);
     } catch (error) {
@@ -156,7 +156,7 @@ router.get('/meetings/closemeeting/:id', async (req, res) => {
  
 router.get('/calls/closecall/:id', async (req, res) => {
     try {
-        const data = await CloseCall.find({userId:req.params.id});
+      
         const data = await CloseCall.find({userId:req.params.id});
         res.status(200).json(data);
     } catch (error) {
