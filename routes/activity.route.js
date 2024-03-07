62918dd1481080584821141928039d32803763cb
shwetabhadke12 <shweta.bhadke@accelgrowthtech.com>
2024-03-07 16:38:09 +0530

    timeline section

--- routes/activity.route.js
+++ routes/activity.route.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const router = express.Router();
 const { Task, Meeting, Call, CloseTask , CloseCall, CloseMeeting} = require('../models/activity.model');
@@ -10,7 +11,7 @@ router.post('/tasks', async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 });
-
+ 
 router.post('/meetings', async (req, res) => {
   try {
     const meeting = await Meeting.create(req.body);
@@ -19,7 +20,7 @@ router.post('/meetings', async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 });
-
+ 
 router.post('/calls', async (req, res) => {
   try {
     const call = await Call.create(req.body);
@@ -28,9 +29,9 @@ router.post('/calls', async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 });
-
-
-
+ 
+ 
+ 
 router.get('/tasks/:id', async (req, res) => {
     try {
         const data = await Task.find({userId:req.params.id});
@@ -40,28 +41,28 @@ router.get('/tasks/:id', async (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
-
-router.get('/meetings', async (req, res) => {
+ 
+router.get('/meetings/:id', async (req, res) => {
     try {
-        const data = await Meeting.find();
+        const data = await Meeting.find({userId:req.params.id});
         res.status(200).json(data);
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
-
-router.get('/calls', async (req, res) => {
+ 
+router.get('/calls/:id', async (req, res) => {
     try {
-        const data = await Call.find();
+        const data = await Call.find({userId:req.params.id});
         res.status(200).json(data);
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
-
-
+ 
+ 
 router.delete('/tasks/:id', async (req, res) => {
     try {
         const { id } = req.params;
@@ -75,7 +76,7 @@ router.delete('/tasks/:id', async (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
-
+ 
 router.delete('/meetings/:id', async (req, res) => {
     try {
         const { id } = req.params;
@@ -110,7 +111,7 @@ router.post('/tasks/closetask', async (req, res) => {
       res.status(400).json({ message: error.message });
     }
   });
-
+ 
   router.post('/meetings/closemeeting', async (req, res) => {
     try {
       const task = await CloseMeeting.create(req.body);
@@ -127,39 +128,82 @@ router.post('/tasks/closetask', async (req, res) => {
       res.status(400).json({ message: error.message });
     }
   });
-
-
-  router.get('/tasks/closetask', async (req, res) => {
+ 
+ 
+  router.get('/tasks/closetask/:id', async (req, res) => {
     try {
-        const data = await CloseTask.find();
+        const data = await CloseTask.find({userId:req.params.id});
         res.status(200).json(data);
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
-
-router.get('/meetings/closemeeting', async (req, res) => {
+ 
+router.get('/meetings/closemeeting/:id', async (req, res) => {
     try {
-        const data = await CloseMeeting.find();
+        const data = await CloseMeeting.find({userId:req.params.id});
         res.status(200).json(data);
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
-
-router.get('/calls/closecall', async (req, res) => {
+ 
+router.get('/calls/closecall/:id', async (req, res) => {
     try {
-        const data = await CloseCall.find();
+        const data = await CloseCall.find({userId:req.params.id});
         res.status(200).json(data);
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
-
-
+ 
+router.delete('/closetasks/:id', async (req, res) => {
+  try {
+      const { id } = req.params;
+      const deletedData = await CloseTask.findByIdAndDelete(id);
+      if (!deletedData) {
+          return res.status(404).json({ error: 'Data not found' });
+      }
+      res.status(200).json({ message: 'Data deleted successfully' });
+  } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+ 
+router.delete('/closemeetings/:id', async (req, res) => {
+  try {
+      const { id } = req.params;
+      const deletedData = await CloseMeeting.findByIdAndDelete(id);
+      if (!deletedData) {
+          return res.status(404).json({ error: 'Data not found' });
+      }
+      res.status(200).json({ message: 'Data deleted successfully' });
+  } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+ 
+ 
+router.delete('/closecalls/:id', async (req, res) => {
+  try {
+      const { id } = req.params;
+      const deletedData = await CloseCall.findByIdAndDelete(id);
+      if (!deletedData) {
+          return res.status(404).json({ error: 'Data not found' });
+      }
+      res.status(200).json({ message: 'Data deleted successfully' });
+  } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+ 
+ 
 const transporter = nodemailer.createTransport({
   host: 'smtp.office365.com',
   port: 587,
@@ -169,11 +213,11 @@ const transporter = nodemailer.createTransport({
     pass: 'LfDAXtcIHIKAC9',
   },
 });
-
+ 
 // Send email
 router.post('/send-email', async (req, res) => {
   const { to, subject, message } = req.body;
-
+ 
   try {
     // Send email
     await transporter.sendMail({
